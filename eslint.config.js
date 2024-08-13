4481c5ad9941dd9526b4f3b67ba2563a017fdba3
koenvanzuijlen <khavanzuijlen@gmail.com>
2024-08-13 17:17:34 +0200

    fix: upgrade dependencies

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["**/.yarn", "**/node_modules", "**/dist"],
+    languageOptions: { globals: { ...globals.node } },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          caughtErrors: "none",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          caughtErrors: "none",
+        },
+      ],
+    },
+  },
+  prettier,
+];
